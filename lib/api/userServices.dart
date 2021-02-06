1cf121e55a573915357b3bbe903378538e1412d7
İsmail Sefa <ismailsefa.akdnz@gmail.com>
2021-02-06 18:04:20 +0300

    added user services (login and register)

---
+++ lib/api/userServices.dart
@@ -0,0 +1,33 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
+class UserServices {
+  static Future signUp(Map body) async {
+    final http.Response response =
+        await http.post('https://flutterappserver.herokuapp.com/users/signup',
+            headers: <String, String>{
+              'Content-Type': 'application/json; charset=UTF-8',
+            },
+            body: jsonEncode(body));
+    if (response.statusCode == 200) {
+      return (jsonDecode(response.body));
+    } else {
+      throw Exception('Failed signUp.');
+    }
+  }
+
+  static Future<Map> signIn(String mail, String password) async {
+    String basicAuth = 'Basic ' + base64Encode(utf8.encode('$mail:$password'));
+    final http.Response response = await http.post(
+        'https://flutterappserver.herokuapp.com/users/signin',
+        headers: <String, String>{
+          'Content-Type': 'application/json; charset=UTF-8',
+          'authorization': basicAuth
+        });
+    if (response.statusCode == 200) {
+      return (jsonDecode(response.body));
+    } else {
+      throw Exception('Failed signIn.');
+    }
+  }
+}
