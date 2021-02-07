aecc6762dbc746db740745b3ca7e69149326e58f
İsmail Sefa <ismailsefa.akdnz@gmail.com>
2021-02-07 19:11:49 +0300

    refactor dashboard

--- lib/api/dashboardServices.dart
+++ lib/api/dashboardServices.dart
@@ -2,7 +2,8 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 
 class DashboardServices {
-  Future<Map> details(String token) async {
+  Future<List> details(String token) async {
+    var countList = new List();
     final http.Response response = await http.get(
         'https://flutterappserver.herokuapp.com/dashboard/details',
         headers: <String, String>{
@@ -11,8 +12,19 @@ class DashboardServices {
         });
     print(response.body);
     if (response.statusCode == 200) {
-      print(jsonDecode(response.body));
-      return (jsonDecode(response.body));
+      var responseMap=jsonDecode(response.body);
+      countList.add(responseMap["wish_count"]);
+      countList.add(responseMap["complaint_count"]);
+      countList.add(responseMap["altyapi_count"]);
+      countList.add(responseMap["ulasim_count"]);
+      countList.add(responseMap["elektrik_count"]);
+      countList.add(responseMap["park_ve_bahceler_count"]);
+      countList.add(responseMap["temizlik_count"]);
+      countList.add(responseMap["resolved_count"]);
+      countList.add(responseMap["denied_count"]);
+      countList.add(responseMap["delayed_count"]);
+      countList.add(responseMap["pending_count"]);
+      return countList;
     } else {
       throw Exception('Failed details.');
     }
