3cbbf966a6b3ef6060c2f9da0c3077cc08e90228
İsmail Sefa <ismailsefa.akdnz@gmail.com>
2021-02-07 16:19:54 +0300

    fixed myRecordList view model

--- lib/api/recordServices.dart
+++ lib/api/recordServices.dart
@@ -20,7 +20,7 @@ class RecordServices {
     }
   }
 
-  Future<List<Map<String,dynamic>>> myRecordList(String token) async {
+  Future<List> myRecordList(String token) async {
     final http.Response response = await http.get(
         'https://flutterappserver.herokuapp.com/user/records',
         headers: <String, String>{
@@ -29,14 +29,13 @@ class RecordServices {
         });
     //print(response.body);
     if (response.statusCode == 200) {
-      print(jsonDecode(response.body));
       return (jsonDecode(response.body));
     } else {
       throw Exception('Failed My Record List.');
     }
   }
 
-  Future<List<Map<String,dynamic>>> districtRecordList(String token) async {
+  Future<List> districtRecordList(String token) async {
     final http.Response response = await http.get(
         'https://flutterappserver.herokuapp.com/record/allFiltered',
         headers: <String, String>{
--- lib/view-models/record_view-model.dart
+++ lib/view-models/record_view-model.dart
@@ -3,34 +3,35 @@ import 'package:flutter_hackathon/api/recordServices.dart';
 import 'package:flutter_hackathon/locator.dart';
 import 'package:flutter_hackathon/models/record.dart';
 
-class RecordViewModel with ChangeNotifier{
+class RecordViewModel with ChangeNotifier {
   RecordServices _recordServices = locator<RecordServices>();
 
   Future<bool> addRecord(Map body, String token) async {
     Map map = await _recordServices.addRecord(body, token);
     notifyListeners();
-    if(map != null){
+    if (map != null) {
       return true;
-    }else{
+    } else {
       return false;
     }
   }
 
   Future<List<Record>> myRecordList(String token) async {
-    List<Map<String, dynamic>> mapRecord = await _recordServices.myRecordList(token);
+    List mapRecord = await _recordServices.myRecordList(token);
     List<Record> myRecords = List<Record>();
-    for(int i=0; i< mapRecord.length; i++)
+    for (int i = 0; i <= mapRecord.length - 1; i++) {
+      myRecords.add(Record.fromJson(mapRecord[i]));
+    }
     print("my record number : ${myRecords.length}");
     return myRecords;
   }
 
   Future<List<Record>> districtRecordList(String token) async {
-    List<Map<String, dynamic>> mapDistrictRecord = await _recordServices.districtRecordList(token);
+    List mapDistrictRecord = await _recordServices.districtRecordList(token);
     List<Record> districtRecords = List<Record>();
-    for(Map<String, dynamic> map in mapDistrictRecord){
-      districtRecords.add(Record.fromJson(map));
+    for (int i = 0; i <= mapDistrictRecord.length - 1; i++) {
+      districtRecords.add(Record.fromJson(mapDistrictRecord[i]));
     }
     return districtRecords;
   }
-
 }
